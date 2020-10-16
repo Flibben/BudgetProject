ed20a348801dbb0db3d421557888845f7e1f2a46
Björn Tirsén <bjorntirsen@gmail.com>
2020-10-16 13:32:40 +0200

    Debugged classes a bit

--- js/interface.js
+++ js/interface.js
@@ -1,8 +1,8 @@
 class Interface {
     constructor() {
-        initialize_create_button();
+        this.initializeCreateButton();
     }
-    initialize_create_button() {
+    initializeCreateButton() {
         let create_button = document.getElementById("newBudgetBtn");
         create_button.addEventListener("click", function(e){
             let user_name = document.getElementById("username");
@@ -10,8 +10,8 @@ class Interface {
             //Make sure user has fillied out the fields
             if ((user_name.value.trim() != "") || (budget_name.value.trim() != "")) {
                 //Create user and budget
-                let (user_name.value + "_user") = new User(user_name.value);
-                (user_name + "_user").createBudget(budget_name.value);
+                let this_user = new User(user_name.value);
+                this_user.createBudget(budget_name.value);
             }
             else {
                 alert("Please enter your name and a name for your budget.");
